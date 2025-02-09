3e5e1d3b131f1580bfadb4ee0e5caafa2ce43b54
Joost <bosjoost201@gmail.com>
2025-02-09 06:32:42 +0100

    chore: cleanup logs

--- resources/js/ziggy.js
+++ resources/js/ziggy.js
@@ -1,5 +1,20 @@
-const Ziggy = {"url":"http:\/\/localhost","port":null,"defaults":{},"routes":{"home":{"uri":"\/","methods":["GET","HEAD"]},"login":{"uri":"login","methods":["GET","HEAD"]},"logout":{"uri":"logout","methods":["GET","HEAD"]},"storage.local":{"uri":"storage\/{path}","methods":["GET","HEAD"],"wheres":{"path":".*"},"parameters":["path"]}}};
-if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-  Object.assign(Ziggy.routes, window.Ziggy.routes);
+const Ziggy = {
+  url: 'http://localhost',
+  port: null,
+  defaults: {},
+  routes: {
+    home: { uri: '/', methods: ['GET', 'HEAD'] },
+    login: { uri: 'login', methods: ['GET', 'HEAD'] },
+    logout: { uri: 'logout', methods: ['GET', 'HEAD'] },
+    'storage.local': {
+      uri: 'storage/{path}',
+      methods: ['GET', 'HEAD'],
+      wheres: { path: '.*' },
+      parameters: ['path'],
+    },
+  },
 }
-export { Ziggy };
+if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
+  Object.assign(Ziggy.routes, window.Ziggy.routes)
+}
+export { Ziggy }
